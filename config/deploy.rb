29934087b3d07c201036600111484c80c3ab58e9
golonyataro <golonya@taro>
2019-10-03 00:59:30 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -46,7 +46,7 @@ lock '3.11.2'
 set :application, 'mini_app_error'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:golonya/mini_app_error.git'
+set :repo_url,  'git@github.com:golonyataro/mini_app_error.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
